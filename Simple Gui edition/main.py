6d407b5ddaa2bc168d323aa5be77a15a0af9a56b
Ctrl_c_n1nja <ffilippov179@gmail.com>
2024-06-09 16:47:54 +0300

    the aswer check is working, sound feature is in development (has issues right now)

--- Simple Gui edition/main.py
+++ Simple Gui edition/main.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as gui
 import  functions
+from playsound import playsound
 
 def main_window():
     layout = [
@@ -27,23 +28,27 @@ def turkish_spelling():
     word = functions.random_word(3)
     layout = [
         [gui.Text("Welcome to the Turkish spelling screen, please select an option")],
-        [gui.Text(word[1]), gui.InputText()],
+        [gui.Text(word[1]), gui.InputText(enable_events=True)],
         [gui.Button("Enter"), gui.Button("Back")],
         ]
-    window = gui.Window("Turkish Spelling", layout)
+    window = gui.Window("Turkish Spelling", layout, return_keyboard_events=True)
     while True:
         event, values = window.read()
         if event == gui.WIN_CLOSED:
             break
         if event == "Back":
             window.close()
+            main_window()
             break
         if event == "Enter" or event == "\r":
             print(values[0])
-            #if functions.perfect_word(values[0]) == functions.perfect_word(word[0]):
+            if functions.perfect_word(values[0]) == functions.perfect_word(word[0]):
+                gui.popup("Correct!")
+                playsound("Simple Gui edition/correct_sound.mp3")
+                #functions.adjust_data(word[0], 'up', 4)
+
+
+
+if __name__ == "__main__":
     main_window()
-
-
-
-main_window()
 
