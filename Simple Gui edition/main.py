64b4ddaafebfcd44842eba55f438c37701d0e68c
Ctrl_c_n1nja <ffilippov179@gmail.com>
2024-06-10 13:47:02 +0300

    weıght actıvated

--- Simple Gui edition/main.py
+++ Simple Gui edition/main.py
@@ -50,13 +50,14 @@ def turkish_spelling():
             if functions.perfect_word(values[0]) == functions.perfect_word(word[0]):
                 pygame.mixer.music.load('correct_sound_1.wav')
                 pygame.mixer.music.play()
-                #functions.adjust_data(word[0], 'up', 4)
+                functions.adjust_data(word[0], 'up', 4)
                 window.close()
                 turkish_spelling()
             else:
                 pygame.mixer.music.load('wrong_answer_1.wav')
                 pygame.mixer.music.play()
                 gui.popup("Incorrect, the correct answer is: " + word[0])
+                functions.adjust_data(word[0], 'down', 4)
                 window.close()
                 turkish_spelling()
 
