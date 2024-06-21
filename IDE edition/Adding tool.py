5dd175beb1d4ec8227d047e944a31503d4313f40
Ctrl_c_n1nja <ffilippov179@gmail.com>
2024-06-21 14:20:52 +0300

    little change

--- IDE edition/Adding tool.py
+++ IDE edition/Adding tool.py
@@ -4,15 +4,27 @@ import os
 os.chdir("..")
 
 data = get_data("tk_words.ods")
-stream = open("words_to_add.txt", "r", encoding="utf-8")
+stream = open("IDE edition/words_to_add.txt", "r", encoding="utf-8")
 print('Are you sure you want to add these words?')
 print('Y/N')
 status = input()
 if status != 'Y':
     exit(0)
-for line in stream:
-    #print([line.split('—')[0][:-1], line.split('—')[1][1:-1], 0, 0])
-    data['Sheet1'].append([line.split('—')[0][:-1], line.split('—')[1][1:-1], 1, 1])
 
+i = 0
+word = []
+for line in stream:
+    if line[-1] == '\n':
+        line = line[:-1]
+    word.append(line)
+    i += 1
+    if i % 2 == 0:
+        print(word)
+        data['Sheet1'].append([word[0], word[1], 1, 1, 1])
+        word = []
+
+if word != []:
+    print(word)
+    data['Sheet1'].append([word[0], word[1], 1, 1, 1])
 save_data("tk_words.ods", data)
 print('Words added successfully!')
